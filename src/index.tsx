dfa3cd6d6a1653c7c94a31e401eb67de5c6aef7c
0918nobita <nobita.0918@gmail.com>
2020-01-14 23:56:41 +0900

    browser-sync の設定を分離し、ServiceWorker を追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,19 @@
 import ReactDOM from '/web_modules/react-dom.js';
 import { jsx } from '/web_modules/@emotion/core.js';
 
+(async (): Promise<void> => {
+  if ('serviceWorker' in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register('lib/serviceWorker.js');
+      registration.update();
+      console.log('serviceWorker registered successfully!');
+    } catch (err) {
+      console.log('ServiceWorker registration failed');
+      console.error(err);
+    }
+  }
+})();
+
 const app = <h2 css={{ color: 'hotpink' }}>Hello World!</h2>;
 
 const elem = document.getElementById('app');
