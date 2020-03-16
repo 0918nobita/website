16422e34a24a6468e6dc0b4cf1464e70d06bbdee
0918nobita <nobita.0918@gmail.com>
2020-03-16 14:49:44 +0900

    :sparkles: フォーム要素を用いたサンプルを追加

--- workspaces/n-mado/src/Main.elm
+++ workspaces/n-mado/src/Main.elm
@@ -1,8 +1,8 @@
 module Main exposing (main)
 
 import Browser exposing (sandbox)
-import Html exposing (Html, button, div, h2, input, p, text)
-import Html.Attributes exposing (placeholder)
+import Html exposing (Html, button, div, form, h2, input, label, p, text)
+import Html.Attributes exposing (for, id, placeholder, style, type_)
 import Html.Events exposing (onClick, onInput)
 
 
@@ -14,12 +14,20 @@ main =
 type alias Model =
     { count : Int
     , content : String
+    , name : String
+    , password : String
+    , passwordAgain : String
     }
 
 
 init : Model
 init =
-    { count = 0, content = "" }
+    { count = 0
+    , content = ""
+    , name = ""
+    , password = ""
+    , passwordAgain = ""
+    }
 
 
 type Msg
@@ -27,6 +35,9 @@ type Msg
     | Decrement
     | Reset
     | Change String
+    | Name String
+    | Password String
+    | PasswordAgain String
 
 
 update : Msg -> Model -> Model
@@ -44,20 +55,88 @@ update msg model =
         Change newContent ->
             { model | content = newContent }
 
+        Name name ->
+            { model | name = name }
+
+        Password password ->
+            { model | password = password }
+
+        PasswordAgain password ->
+            { model | passwordAgain = password }
+
 
 view : Model -> Html Msg
 view model =
     div []
-        [ div []
-            [ h2 [] [ text "Counter" ]
-            , p [] [ text <| String.fromInt model.count ]
-            , button [ onClick Increment ] [ text "+" ]
-            , button [ onClick Decrement ] [ text "-" ]
-            , button [ onClick Reset ] [ text "reset" ]
-            ]
-        , div []
-            [ h2 [] [ text "Text Reverser" ]
-            , input [ onInput Change, placeholder "Text to reverse" ] []
-            , p [] [ text <| "Result: " ++ String.reverse model.content ]
+        [ h2 [] [ text "Counter" ]
+        , p [] [ text <| String.fromInt model.count ]
+        , button [ onClick Increment ] [ text "+" ]
+        , button [ onClick Decrement ] [ text "-" ]
+        , button [ onClick Reset ] [ text "reset" ]
+        , h2 [] [ text "Text Reverser" ]
+        , input [ onInput Change, placeholder "Text to reverse" ] []
+        , p [] [ text <| "Result: " ++ String.reverse model.content ]
+        , h2 [] [ text "Form" ]
+        , form []
+            [ viewInput NameForm
+            , viewInput PasswordForm
+            , viewInput PasswordAgainForm
+            , viewValidation model
             ]
         ]
+
+
+type FormKind
+    = NameForm
+    | PasswordForm
+    | PasswordAgainForm
+
+
+viewInput : FormKind -> Html Msg
+viewInput formKind =
+    case formKind of
+        NameForm ->
+            div []
+                [ label [ for "name" ] [ text "Name" ]
+                , input
+                    [ id "name"
+                    , type_ "text"
+                    , onInput Name
+                    ]
+                    []
+                ]
+
+        PasswordForm ->
+            div []
+                [ label [ for "password" ] [ text "Password" ]
+                , input
+                    [ id "password"
+                    , type_ "password"
+                    , onInput Password
+                    ]
+                    []
+                ]
+
+        PasswordAgainForm ->
+            div []
+                [ label [ for "passwordAgain" ] [ text "Password (Again)" ]
+                , input
+                    [ id "passwordAgain"
+                    , type_ "password"
+                    , onInput PasswordAgain
+                    ]
+                    []
+                ]
+
+
+viewValidation : Model -> Html msg
+viewValidation model =
+    if model.passwordAgain /= "" then
+        if model.password == model.passwordAgain then
+            div [ style "color" "green" ] [ text "OK" ]
+
+        else
+            div [ style "color" "red" ] [ text "Passwords do not match!" ]
+
+    else
+        div [] []
