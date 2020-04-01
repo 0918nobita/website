d22fd1ecb37a15e4ac05fa6a6c5de866a68d6886
0918nobita <nobita.0918@gmail.com>
2020-04-01 17:34:51 +0900

    :sparkles: http リクエストを飛ばすサンプルを追加

--- workspaces/client/main.rkt
+++ workspaces/client/main.rkt
@@ -1,5 +1,15 @@
 #lang racket/base
 
-(require http/request)
+(require (only-in racket/list
+                  empty))
 
-connect
+(require (only-in http/request
+                  call/input-request
+                  read-entity/bytes))
+
+(display
+  (call/input-request "1.1"
+                      "GET"
+                      "https://kodai.vision"
+                      empty
+                      read-entity/bytes))
