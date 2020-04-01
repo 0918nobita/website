cbe3c37b9a879dc999ea6c8e78faf0992242b660
0918nobita <nobita.0918@gmail.com>
2020-04-01 22:38:54 +0900

    :sparkles: CDA にリクエストを飛ばすサンプルを追加

---
+++ workspaces/client/README.md
@@ -0,0 +1,10 @@
+# HTTP Client
+
+## `run.sh`
+
+```bash
+SPACE_ID=... \
+ENV_ID=... \
+ACCESS_TOKEN=... \
+racket main.rkt
+```
--- workspaces/client/main.rkt
+++ workspaces/client/main.rkt
@@ -1,5 +1,16 @@
 #lang racket/base
 
+(require (only-in json
+                  bytes->jsexpr
+                  jsexpr->string))
+
+(require (only-in net/url-string
+                  url->string))
+
+(require (only-in net/url-structs
+                  path/param
+                  url))
+
 (require (only-in racket/list
                   empty))
 
@@ -7,9 +18,31 @@
                   call/input-request
                   read-entity/bytes))
 
-(display
-  (call/input-request "1.1"
-                      "GET"
-                      "https://kodai.vision"
-                      empty
-                      read-entity/bytes))
+(define uri
+  (url->string
+    (url
+      "https"
+      #f
+      "cdn.contentful.com"
+      #f
+      #t
+      `(,(path/param "spaces" empty)
+        ,(path/param (getenv "SPACE_ID") empty)
+        ,(path/param "environments" empty)
+        ,(path/param (getenv "ENV_ID") empty)
+        ,(path/param "entries" empty))
+      `((access_token . ,(getenv "ACCESS_TOKEN")))
+      #f)))
+
+(displayln
+  (hash-ref
+    (hash-ref
+      (bytes->jsexpr
+        (call/input-request
+          "1.1"
+          "GET"
+          uri
+          empty
+          read-entity/bytes))
+      'sys)
+    'type))
