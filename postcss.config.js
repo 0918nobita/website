9acc941c5d848b1a75fb23ed38dc292a0e41082a
0918nobita <nobita.0918@gmail.com>
2020-04-27 12:16:45 +0900

    :wrench: postcss-flexbugs-fixes, postcss-preset-env を利用するための設定を追加

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-    plugins: ['autoprefixer', 'cssnano'],
+    plugins: [
+        'postcss-flexbugs-fixes',
+        [
+            'postcss-preset-env',
+            {
+                autoprefixer: {
+                    flexbox: 'no-2009',
+                },
+                stage: 3,
+                features: {
+                    'custom-properties': false,
+                },
+            },
+        ],
+        'cssnano',
+    ],
 };
