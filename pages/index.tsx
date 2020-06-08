9c26a56018d3339d9699432f8161ebdd42a66463
0918nobita <nobita.0918@gmail.com>
2020-06-08 13:29:54 +0900

    :sparkles: Tailwind CSS を用いたサンプルを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,19 @@ import React from 'react';
 
 import { Button } from '../components/Button';
 
+type Item = React.FC<{ first?: boolean }>;
+
+const Item: Item = ({ first, children }) => {
+    const className = `block text-gray-700 text-center bg-gray-400 px-4 py-2${
+        first ? '' : ' mt-2'
+    }`;
+    return <span className={className}>{children}</span>;
+};
+
+Item.defaultProps = {
+    first: false,
+};
+
 const IndexPage: React.FC = () => (
     <>
         <h2>Hello, Next.js!</h2>
@@ -10,6 +23,11 @@ const IndexPage: React.FC = () => (
         <Link href="/articles">
             <a>Articles</a>
         </Link>
+        <div className="bg-gray-200 p-4">
+            <Item first>1</Item>
+            <Item>2</Item>
+            <Item>3</Item>
+        </div>
     </>
 );
 
