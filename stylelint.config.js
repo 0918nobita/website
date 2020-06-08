1d16b7d3fd0b08592146513cfea1acca15a90055
0918nobita <nobita.0918@gmail.com>
2020-06-08 13:09:40 +0900

    :wrench: Stylelint のルール設定を修正

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,3 +1,12 @@
 module.exports = {
     extends: 'stylelint-config-standard',
+    rules: {
+        indentation: 4,
+        'at-rule-no-unknown': [
+            true,
+            {
+                ignoreAtRules: ['tailwind'],
+            },
+        ],
+    },
 };
