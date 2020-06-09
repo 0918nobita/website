558a437759e9e18865b1f03f218680477e832328
0918nobita <nobita.0918@gmail.com>
2020-06-09 23:46:11 +0900

    :construction: Layout コンポーネントを追加

---
+++ components/Layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './Layout.module.css';
+
+const Layout: React.FC = () => (
+    <>
+        <div className={styles.menu}></div>
+    </>
+);
+
+export default Layout;
