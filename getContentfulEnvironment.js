f59fe238a566f6907ff6c0c7a6d0fd630022d9ce
0918nobita <nobita.0918@gmail.com>
2020-06-09 15:19:43 +0900

    :art: Format code

--- getContentfulEnvironment.js
+++ getContentfulEnvironment.js
@@ -2,10 +2,12 @@ const contentfulManagement = require('contentful-management');
 
 require('dotenv').config();
 
-module.exports = function() {
+module.exports = function () {
     const contentfulClient = contentfulManagement.createClient({
         accessToken: process.env.MANAGEMENT_AT,
     });
 
-    return contentfulClient.getSpace(process.env.SPACE).then(space => space.getEnvironment(process.env.ENV))
+    return contentfulClient
+        .getSpace(process.env.SPACE)
+        .then((space) => space.getEnvironment(process.env.ENV));
 };
