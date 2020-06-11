92327cf50d661f5c0be045ad250a0f59d439d652
0918nobita <nobita.0918@gmail.com>
2020-06-11 19:17:06 +0900

    :sparkles: 立方体が回転するアニメーションを追加

--- components/Three.tsx
+++ components/Three.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import { PerspectiveCamera, Scene, WebGLRenderer } from 'three';
+import {
+    BoxGeometry,
+    DirectionalLight,
+    Mesh,
+    MeshStandardMaterial,
+    PerspectiveCamera,
+    Scene,
+    WebGLRenderer,
+} from 'three';
 
 import { useWindowSize } from './hooks/useWindowSize';
 
@@ -12,16 +20,39 @@ const Three: React.FC = () => {
         const scene = new Scene();
 
         const camera = new PerspectiveCamera(
-            75,
+            45,
             canvas.clientWidth / canvas.clientHeight,
-            0.1,
-            1000
+            1,
+            10000
         );
+        camera.position.set(0, 0, 1000);
+
+        const geometry = new BoxGeometry(500, 500, 500);
+        const material = new MeshStandardMaterial({ color: 0x0000ff });
+        const box = new Mesh(geometry, material);
+        scene.add(box);
+
+        const light = new DirectionalLight(0xffffff);
+        light.intensity = 2;
+        light.position.set(1, 1, 1);
+        scene.add(light);
 
         const renderer = new WebGLRenderer({ canvas, antialias: true });
         renderer.setClearColor('#1d1d1d');
         renderer.setSize(width, height);
+        renderer.setPixelRatio(window.devicePixelRatio);
         renderer.render(scene, camera);
+
+        tick();
+
+        function tick(): void {
+            window.requestAnimationFrame(tick);
+
+            box.rotation.x += 0.01;
+            box.rotation.y += 0.01;
+
+            renderer.render(scene, camera);
+        }
     };
 
     return (
