82ec1c5fc57f38e303095427880da75dc40f01c4
0918nobita <nobita.0918@gmail.com>
2022-01-18 01:28:19 +0900

    :recycle: Do refactor

---
+++ components/server/src/lib.rs
@@ -0,0 +1,81 @@
+use actix_files::Files;
+use actix_web::{
+    dev::Server,
+    http::{header, ContentEncoding},
+    middleware::{Compress, Logger},
+    web, App, HttpRequest, HttpResponse, HttpServer,
+};
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+use serde::Deserialize;
+
+fn default_www_root() -> String {
+    "./public".to_owned()
+}
+
+fn default_certificate_path() -> String {
+    "localhost.pem".to_owned()
+}
+
+fn default_private_key_path() -> String {
+    "localhost-key.pem".to_owned()
+}
+
+#[derive(Deserialize)]
+pub struct Config {
+    #[serde(default = "default_www_root")]
+    www_root: String,
+
+    #[serde(default = "default_certificate_path")]
+    certificate_path: String,
+
+    #[serde(default = "default_private_key_path")]
+    private_key_path: String,
+}
+
+pub fn http_server(http_addr: &str) -> Server {
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .default_service(web::route().to(|req: HttpRequest| {
+                let host = req.connection_info().host().to_owned();
+                let uri = req.uri();
+                let url = format!("https://{}{}", host, uri);
+                HttpResponse::TemporaryRedirect()
+                    .header(header::LOCATION, url)
+                    .finish()
+                    .into_body()
+            }))
+    })
+    .workers(2)
+    .bind(http_addr)
+    .expect(&format!("Failed to bind {}", http_addr))
+    .run()
+}
+
+pub fn https_server(https_addr: &str, config: Config) -> Server {
+    let Config {
+        www_root,
+        certificate_path,
+        private_key_path,
+    } = config;
+
+    let mut ssl = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    ssl.set_certificate_chain_file(certificate_path).unwrap();
+    ssl.set_private_key_file(private_key_path, SslFiletype::PEM)
+        .unwrap();
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Compress::new(ContentEncoding::Br))
+            .wrap(Logger::default())
+            .service(Files::new("/", www_root.clone()).index_file("index.html"))
+            .default_service(web::route().to(|| {
+                HttpResponse::NotFound()
+                    .content_type("text/html")
+                    .body(include_str!("./404.html"))
+            }))
+    })
+    .bind_openssl(https_addr, ssl)
+    .expect(&format!("Failed to bind {}", https_addr))
+    .run()
+}
--- components/server/src/main.rs
+++ components/server/src/main.rs
@@ -1,84 +1,21 @@
-use actix_files::Files;
+extern crate server;
+
 use actix_rt::System;
-use actix_web::{
-    http::{header, ContentEncoding},
-    middleware::{Compress, Logger},
-    web, App, HttpRequest, HttpResponse, HttpServer,
-};
 use futures::future;
-use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
-use serde::Deserialize;
-
-fn default_www_root() -> String {
-    "./public".to_owned()
-}
-
-fn default_certificate_path() -> String {
-    "localhost.pem".to_owned()
-}
-
-fn default_private_key_path() -> String {
-    "localhost-key.pem".to_owned()
-}
-
-#[derive(Deserialize)]
-struct Config {
-    #[serde(default = "default_www_root")]
-    www_root: String,
-
-    #[serde(default = "default_certificate_path")]
-    certificate_path: String,
-
-    #[serde(default = "default_private_key_path")]
-    private_key_path: String,
-}
+use server::{http_server, https_server, Config};
 
 fn main() -> anyhow::Result<()> {
     env_logger::init();
 
     let config: Config = envy::from_env()?;
 
-    let mut ssl = SslAcceptor::mozilla_intermediate(SslMethod::tls())?;
-    ssl.set_certificate_chain_file(config.certificate_path)?;
-    ssl.set_private_key_file(config.private_key_path, SslFiletype::PEM)?;
-
-    let www_root = config.www_root;
-
     let mut sys_runner = System::new("web-server");
-    let _ = sys_runner.block_on(async {
+    let _ = sys_runner.block_on(async move {
         let http_addr = "0.0.0.0:80";
-        let http = HttpServer::new(|| {
-            App::new()
-                .wrap(Logger::default())
-                .default_service(web::route().to(|req: HttpRequest| {
-                    let host = req.connection_info().host().to_owned();
-                    let uri = req.uri();
-                    let url = format!("https://{}{}", host, uri);
-                    HttpResponse::TemporaryRedirect()
-                        .header(header::LOCATION, url)
-                        .finish()
-                        .into_body()
-                }))
-        })
-        .bind(http_addr)
-        .expect(&format!("Failed to bind {}", http_addr))
-        .run();
+        let http = http_server(http_addr);
 
         let https_addr = "0.0.0.0:443";
-        let https = HttpServer::new(move || {
-            App::new()
-                .wrap(Compress::new(ContentEncoding::Br))
-                .wrap(Logger::default())
-                .service(Files::new("/", www_root.clone()).index_file("index.html"))
-                .default_service(web::route().to(|| {
-                    HttpResponse::NotFound()
-                        .content_type("text/html")
-                        .body(include_str!("./404.html"))
-                }))
-        })
-        .bind_openssl(https_addr, ssl)
-        .expect(&format!("Failed to bind {}", https_addr))
-        .run();
+        let https = https_server(https_addr, config);
 
         future::try_join(http, https).await
     });
