4518599a18e2cf2718a1a52ea0e965460410e573
0918nobita <nobita.0918@gmail.com>
2022-01-17 15:05:29 +0900

    :wrench: Web サーバーを起動したままにするためのデーモンの設定を Playbook に追記した

--- conf/setup-service.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -ex
-
-ln -s /home/kodai/kodai-blog/conf/launch-server.sh /opt/launch-server.sh
-
-ln -s /home/kodai/kodai-blog/conf/kodai-blog.service /etc/systemd/system/kodai-blog.service
-
-systemctl enable kodai-blog
-
-systemctl start kodai-blog
