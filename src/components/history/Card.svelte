c5227046b1f1139be4ae9398c532799e0e60f6e4
0918nobita <nobita.0918@gmail.com>
2024-04-13 22:24:10 +0900

    自分史ページで、Subgrid でレイアウトしたカードを表示するようにした

---
+++ src/components/history/Card.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+  export let title: string;
+  export let desc: string;
+
+  export let hasDialog = false;
+
+  function onClick() {
+    if (!hasDialog) {
+      return;
+    }
+    // console.log('clicked');
+  }
+</script>
+
+{#if hasDialog}
+  <button type="button" on:click={onClick}>
+    <div class="title"><h3>{title}</h3></div>
+    <p>{desc}</p>
+  </button>
+{:else}
+  <article>
+    <div class="title"><h3>{title}</h3></div>
+    <p>{desc}</p>
+  </article>
+{/if}
+
+<style>
+  button {
+    display: grid;
+    grid-template-rows: subgrid;
+    grid-row: span 2;
+    gap: 0;
+    border-radius: 7px;
+    border: 2px solid var(--card-border-color);
+    box-shadow: var(--card-box-shadow);
+    background: transparent;
+    appearance: none;
+    padding: 0;
+    color: var(--main-text);
+    font-size: 15px;
+    line-height: 1.5;
+    text-align: start;
+  }
+
+  article {
+    display: grid;
+    grid-template-rows: subgrid;
+    grid-row: span 2;
+    gap: 0;
+    border-radius: 7px;
+    border: 2px solid var(--card-border-color);
+    box-shadow: var(--card-box-shadow);
+    font-size: 15px;
+    line-height: 1.5;
+  }
+
+  .title {
+    margin: 0;
+    background-color: var(--card-border-color);
+    padding: 0.5rem 0.7rem 0.5rem 0.7rem;
+    display: grid;
+    align-items: center;
+  }
+
+  h3 {
+    margin: 0;
+  }
+
+  p {
+    margin: 0;
+    padding: 0.5rem 0.7rem 0.5rem 0.7rem;
+  }
+</style>
---
+++ src/components/history/HistoryCards.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+</script>
+
+<div class="card-container">
+  <slot />
+</div>
+
+<style>
+  .card-container {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(270px, 1fr));
+    gap: 30px;
+  }
+</style>
