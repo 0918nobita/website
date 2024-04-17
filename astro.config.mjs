9d56601e147372ea0bf46e98fdbdab1fd8cd484d
0918nobita <nobita.0918@gmail.com>
2024-04-17 17:12:17 +0900

    robots.txt, sitemap を追加した

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import { defineConfig } from 'astro/config';
@@ -6,7 +7,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://kodai.engineer',
-    integrations: [svelte(), mdx()],
+    integrations: [svelte(), mdx(), sitemap()],
     vite: {
         plugins: [
             vanillaExtractPlugin({
