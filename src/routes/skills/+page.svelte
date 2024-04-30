046059c1cc726963b2ac508f133ca32fd056bd33
Kodai Matsumoto <nobita.0918@gmail.com>
2024-04-30 12:54:09 +0900

    スキルについて追記した

--- src/routes/skills/+page.svelte
+++ src/routes/skills/+page.svelte
@@ -14,23 +14,52 @@
 
 <h1>スキル・強み</h1>
 <h2>Web 開発</h2>
+<h3>HTML/CSS</h3>
+<p>
+  中学２年生の頃にホームページを作るという目的で入門し、現在まで頻繁に触れてきた技術です。
+</p>
+<p>
+  現在は WAI-ARIA, WCAG
+  を意識したマークアップを学びつつ個人開発で実践しています。
+</p>
+<p>その他、以下のような関連技術を学んできました。</p>
+<ul>
+  <li>Media Query, CSS 変数を活用したダークモード対応</li>
+  <li>
+    Flexbox, Grid Layout, Subgrid, Media/Container Query
+    を用いたレスポンシブ対応
+  </li>
+  <li>
+    <code>@layer</code> ルール (Cascade Layers) を用いた、スタイル適用順序の制御
+  </li>
+</ul>
+<h3>JavaScript / TypeScript</h3>
 <ul>
   <li>
-    HTML / CSS
+    Vanilla Extract CSS
     <ul>
-      <li>ダークモード対応</li>
       <li>
-        Flexbox, Grid Layout, Subgrid, Media/Container Query
-        を用いたレスポンシブ対応
+        TypeScript コードとしてスタイルシートを記述し、ビルド時に対応する静的な
+        CSS を生成できます。
       </li>
       <li>
-        <code>@layer</code> ルール (Cascade Layers) を用いた、スタイル適用順序の制御
+        TypeScript
+        の型検査器を活かして「誤ったプロパティ名を記述してしまう」「未定義の CSS
+        変数を参照してしまう」等のミスを防ぐことができます。
       </li>
     </ul>
   </li>
-  <li>JavaScript / TypeScript</li>
-  <li>Vanilla Extract CSS</li>
-  <li>Zod</li>
+  <li>
+    Zod
+    <ul>
+      <li>実行時のバリデーションや TS の型定義の生成に利用しています</li>
+      <li>
+        このウェブサイトの一部ページは Markdown
+        文書から静的に生成されていますが、その Frontmatter
+        の検証に利用しています。
+      </li>
+    </ul>
+  </li>
   <li>Svelte, SvelteKit</li>
   <li>Astro</li>
   <li>React</li>
@@ -40,7 +69,10 @@
   <li>RxJS</li>
   <li>Elm</li>
 </ul>
-<h2>その他</h2>
+<h2>その他の、これまで触れてきた言語</h2>
+<p>
+  プログラミング言語そのものや、言語仕様の背景にある思想を知ることが大好きなので、これまで沢山のプログラミング言語に触れてきました
+</p>
 <ul>
   <li>Java</li>
   <li>Kotlin</li>
@@ -48,5 +80,9 @@
   <li>OCaml</li>
   <li>Haskell</li>
   <li>Rust</li>
+  <li>FORTH</li>
+  <li>Common Lisp</li>
+  <li>Scheme / Racket</li>
+  <li>Lua</li>
   <li>Gleam</li>
 </ul>
