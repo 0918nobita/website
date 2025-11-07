9abf788d93d820b0dfe875f1e58b41dd11a44bc3
0918nobita <nobita.0918@gmail.com>
2025-11-08 00:29:53 +0900

    ThemeToggle の使い方をシンプルにした

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,32 +1,26 @@
 <script lang="ts">
-import '~/style/global.css';
-import '~/style/layer.css';
+  import "~/style/global.css";
+  import "~/style/layer.css";
 
-import { Sidebar, SpHeader, ThemeToggleSkeleton } from '~/components';
+  import { Sidebar, SpHeader, ThemeToggle } from "~/components";
 
-import { useThemeToggle } from './useThemeToggle';
-import { useViewTransition } from './useViewTransition';
+  import { useThemeToggle } from "./useThemeToggle";
+  import { useViewTransition } from "./useViewTransition";
 
-import * as styles from './layout.css';
+  import * as styles from "./layout.css";
 
-const { themeTogglePromise, currentlyAppliedTheme, themeSetting, onChange } =
-  useThemeToggle();
+  const { currentlyAppliedTheme, themeSetting } = useThemeToggle();
 
-useViewTransition();
+  useViewTransition();
 </script>
 
 <main class={styles.main}>
   <!-- 画面の横幅にあわせて Sidebar と SpHeader のどちらかが表示される -->
   <Sidebar className={styles.sidebar}>
-    {#await themeTogglePromise}
-      <ThemeToggleSkeleton />
-    {:then ThemeToggle}
-      <ThemeToggle
-        currentlyAppliedTheme={$currentlyAppliedTheme}
-        bind:themeSetting={$themeSetting}
-        {onChange}
-      />
-    {/await}
+    <ThemeToggle
+      currentlyAppliedTheme={$currentlyAppliedTheme}
+      bind:themeSetting={$themeSetting}
+    />
   </Sidebar>
 
   <SpHeader className={styles.spHeader} />
--- src/routes/useThemeToggle.ts
+++ src/routes/useThemeToggle.ts
@@ -1,76 +1,64 @@
-import { onMount, type ComponentType } from 'svelte';
-import { get, writable } from 'svelte/store';
+import { onMount } from "svelte";
+import { writable } from "svelte/store";
 
 const osThemeSetting = () =>
-    window.matchMedia('(prefers-color-scheme: dark)').matches
-        ? 'dark'
-        : 'light';
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light";
 
 export const useThemeToggle = () => {
-    const currentlyAppliedTheme = writable<'dark' | 'light'>('light');
-    const themeSetting = writable<'dark' | 'light' | 'match-system'>(
-        'match-system',
+    const currentlyAppliedTheme = writable<"dark" | "light">("light");
+    const themeSetting = writable<"dark" | "light" | "match-system">(
+        "match-system",
     );
 
-    const onChange = () => {
-        switch (get(themeSetting)) {
-            case 'dark':
-                document.documentElement.classList.remove('light');
-                document.documentElement.classList.add('dark');
-                currentlyAppliedTheme.set('dark');
-                localStorage.setItem('theme', 'dark');
-                return;
-            case 'light':
-                document.documentElement.classList.remove('dark');
-                document.documentElement.classList.add('light');
-                currentlyAppliedTheme.set('light');
-                localStorage.setItem('theme', 'light');
-                return;
-        }
-
-        localStorage.removeItem('theme');
-
-        const defaultTheme = osThemeSetting();
-
-        if (defaultTheme === 'dark') {
-            document.documentElement.classList.remove('light');
-            document.documentElement.classList.add('dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-            document.documentElement.classList.add('light');
-        }
-
-        currentlyAppliedTheme.set(defaultTheme);
-    };
-
-    let resolveThemeToggle: (themeToggle: ComponentType) => void;
-    const themeTogglePromise = new Promise<ComponentType>((r) => {
-        resolveThemeToggle = r;
-    });
-
-    onMount(async () => {
+    onMount(() => {
         currentlyAppliedTheme.set(window.initialTheme);
 
-        const theme = localStorage.getItem('theme');
+        const theme = localStorage.getItem("theme");
 
-        if (theme === 'dark') {
-            themeSetting.set('dark');
-        } else if (theme === 'light') {
-            themeSetting.set('light');
+        if (theme === "dark") {
+            themeSetting.set("dark");
+        } else if (theme === "light") {
+            themeSetting.set("light");
         } else {
-            themeSetting.set('match-system');
+            themeSetting.set("match-system");
         }
 
-        const { default: ThemeToggle } = await import(
-            '~/components/ThemeToggle/ThemeToggle.svelte'
-        );
-        resolveThemeToggle(ThemeToggle);
+        themeSetting.subscribe((newSetting) => {
+            switch (newSetting) {
+                case "dark":
+                    document.documentElement.classList.remove("light");
+                    document.documentElement.classList.add("dark");
+                    currentlyAppliedTheme.set("dark");
+                    localStorage.setItem("theme", "dark");
+                    return;
+                case "light":
+                    document.documentElement.classList.remove("dark");
+                    document.documentElement.classList.add("light");
+                    currentlyAppliedTheme.set("light");
+                    localStorage.setItem("theme", "light");
+                    return;
+            }
+
+            localStorage.removeItem("theme");
+
+            const defaultTheme = osThemeSetting();
+
+            if (defaultTheme === "dark") {
+                document.documentElement.classList.remove("light");
+                document.documentElement.classList.add("dark");
+            } else {
+                document.documentElement.classList.remove("dark");
+                document.documentElement.classList.add("light");
+            }
+
+            currentlyAppliedTheme.set(defaultTheme);
+        });
     });
 
     return {
-        themeTogglePromise,
         currentlyAppliedTheme,
         themeSetting,
-        onChange,
     };
 };
